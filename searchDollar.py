341d98c710b121683c29fcc2204baed0c6f6874b
HeitorDalla <heitorvillavilla@gmail.com>
2025-06-19 18:07:57 -0300

    Criando robo para verificar a cotacao do dolar

---
+++ searchDollar.py
@@ -0,0 +1,25 @@
+import pyautogui as mousePosition #biblioteca para controlar as ações com o mouse e teclado
+
+mousePosition.hotkey('win')
+
+mousePosition.typewrite('cmd')
+
+mousePosition.press('enter')
+
+mousePosition.sleep(1)
+
+mousePosition.typewrite('start chrome')
+
+mousePosition.press('enter')
+
+mousePosition.sleep(1)
+
+mousePosition.typewrite('https://google.com/')
+
+mousePosition.press('enter')
+
+mousePosition.sleep(2)
+
+mousePosition.typewrite('Dolar')
+
+mousePosition.press('enter')
