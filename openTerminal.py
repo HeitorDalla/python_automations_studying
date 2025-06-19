75903cf453483dc3bd2c4bb84977e7b6b5f561f0
HeitorDalla <heitorvillavilla@gmail.com>
2025-06-19 15:43:31 -0300

    Primeira automacao, abrindo o terminal com script

---
+++ openTerminal.py
@@ -0,0 +1,12 @@
+import pyautogui as mousePosition
+
+print(mousePosition.position())
+
+mousePosition.moveTo(x=2930, y=1598)
+mousePosition.click(x=2930, y=1598)
+
+mousePosition.typewrite("cmd")
+
+mousePosition.sleep(2)
+
+mousePosition.click(x=3107, y=744)
